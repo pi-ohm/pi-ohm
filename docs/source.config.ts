f644fc672432fb7c1556bfd9a82ac442b30793ce
cau1k <zero@composer.group>
2026-02-24 21:05:03 -0500

    chore(docs): styles

--- docs/source.config.ts
+++ docs/source.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
   mdxOptions: {
     rehypeCodeOptions: {
       themes: {
-        light: "everforest-light",
-        dark: "everforest-dark",
+        light: "vitesse-light",
+        dark: "vitesse-dark",
       },
     },
   },
